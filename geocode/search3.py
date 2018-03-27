c050116329a793974810e0908becadfc559b7b53
Jack Lorente <lorente.sociologia@gmail.com>
2018-03-27 20:01:47 -0300

    search 2 e 3

---
+++ geocode/search3.py
@@ -0,0 +1,19 @@
+#usr/bin/env python
+#Programação de redes com python - trabalhando http (descendo na pilha, do request para o http)
+
+import http.client
+import json
+from urllib.parse import quote_plus
+
+base = '/maps/api/geocode/json'
+
+def geocode(address):
+    path = '{}?address={}&sensor=false'.format(base, quote_plus(address))
+    connection = http.client.HTTPConnection('maps.google.com')
+    connection.request('GET', path)
+    rawreply = connection.getresponse().read()
+    reply = json.loads(rawreply.decode('utf-8'))
+    print(reply['results'][0]['geometry']['location'])
+
+if __name__ == '__main__':
+	geocode('Julio Sayago, 405, Sao Paulo, SP')
